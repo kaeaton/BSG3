496505e35b64822adc219295ad7e93acf0583035
Katrina Eaton <phrynne@gmail.com>
2017-12-06 09:23:47 -0800

    Separate locus parser into own class

---
+++ b12x-mvn/src/main/java/b12x/LocusParser.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b12x;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author kaeaton
+ */
+public class LocusParser {
+    public LocusParser (){
+
+    }
+    
+    public String parseLocus(String locus){
+        String parsedLocus = "";
+        Pattern p = Pattern.compile("^HLA-(\\w+)$");
+        Matcher m = p.matcher(locus);
+            
+        // Find locus
+        if (m.find()) {
+            parsedLocus = m.group(1);
+            System.out.println(parsedLocus);
+        }
+        
+        return parsedLocus;
+    }
+}
